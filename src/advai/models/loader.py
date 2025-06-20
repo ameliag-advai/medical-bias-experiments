0647fb95936d25f75882e864d55eca9e61621411
jakethomas12 <jake.thomas@advai.co.uk>
2025-06-20 15:24:48 +0100

    Fix model loading to incorrect device

--- src/advai/models/loader.py
+++ src/advai/models/loader.py
@@ -8,12 +8,12 @@ def load_model_and_sae(model_scope="gemma", device=None):
     if device is None:
         device = "cuda" if torch.cuda.is_available() else "cpu"
 
-
-    model = HookedTransformer.from_pretrained("google/gemma-2b-it", device=device)
+    model = HookedTransformer.from_pretrained("google/gemma-2b-it", device=device, move_to_device=True)
     sae, *_ = SAE.from_pretrained(
         release="jbloom/Gemma-2b-IT-Residual-Stream-SAEs",
         sae_id="gemma_2b_it_blocks.12.hook_resid_post_16384",
         device=device
     )
+    model.to(device)
 
     return model, sae
