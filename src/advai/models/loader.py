02107eff9743992fafe5ccd9c00517c36cb6b83b
Amelia G <162914886+Amelia3141@users.noreply.github.com>
2025-06-28 18:24:50 +0100

    sae reconstruction fix

--- src/advai/models/loader.py
+++ src/advai/models/loader.py
@@ -10,8 +10,8 @@ def load_model_and_sae(model_scope="gemma", device=None):
 
     model = HookedTransformer.from_pretrained("google/gemma-2b-it", device=device, move_to_device=True)
     sae, *_ = SAE.from_pretrained(
-        release="jbloom/Gemma-2b-IT-Residual-Stream-SAEs",
-        sae_id="gemma_2b_it_blocks.12.hook_resid_post_16384",
+        release="gemma-2b-it-res-jb",
+        sae_id="blocks.12.hook_resid_post",
         device=device
     )
     model.to(device)
